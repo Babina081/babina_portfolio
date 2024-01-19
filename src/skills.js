95a9671f628a58f60dd63258ea9f06034d82882c
Babina081 <babitamoo333@gmail.com>
2024-01-19 12:16:23 +0545

    updating ....

--- src/skills.js
+++ src/skills.js
@@ -1,91 +1,93 @@
-const skills=[
-    {
-    id:1,
-    name:"HTML",
-    skill:80,
-},
-    {
-    id:2,
-    name:"CSS",
-    skill:80,
-},
-    {
-    id:3,
-    name:"Javascript",
-    skill:70
-},
-    {
-    id:4,
-    name:"Bootstrap",
-    skill:60
-},
-    {
-    id:5,
-    name:"ReactJs",
-    skill:70
-},
-    {
-    id:6,
-    name:"ES6",
-    skill:60
-},
-    {
-    id:7,
-    name:"jQuery",
-    skill:60
-},
-    {
-    id:8,
-    name:"EJS",
-    skill:60
-},
-   
-]
+const skills = [
+  {
+    id: 1,
+    name: "HTML",
+    skill: 80,
+  },
+  {
+    id: 2,
+    name: "CSS",
+    skill: 80,
+  },
+  {
+    id: 3,
+    name: "Javascript",
+    skill: 70,
+  },
+  {
+    id: 4,
+    name: "Bootstrap",
+    skill: 60,
+  },
+  {
+    id: 5,
+    name: "ReactJs",
+    skill: 70,
+  },
+  {
+    id: 6,
+    name: "ES6",
+    skill: 60,
+  },
+  {
+    id: 7,
+    name: "jQuery",
+    skill: 60,
+  },
+  {
+    id: 8,
+    name: "EJS",
+    skill: 60,
+  },
+  {
+    id: 9,
+    name: "Canva",
+    skill: 60,
+  },
+];
 
-const backend_skills=[    {
-    id:1,
-    name:"Javascript",
-    skill:70,
-},
-    {
-    id:2,
-    name:"Nodejs",
-    skill:70,
-},
-    {
-    id:3,
-    name:"ExpressJs",
-    skill:60
-},
-    {
-    id:4,
-    name:"Git",
-    skill:70
-},
-    {
-    id:5,
-    name:"PostgreSQl",
-    skill:50
-},
-    {
-    id:6,
-    name:"MongoDB",
-    skill:50
-},
-    {
-    id:7,
-    name:"Canva",
-    skill:60
-},
-    {
-    id:8,
-    name:"Postman",
-    skill:50
-},
-    {
-    id:8,
-    name:"RestAPI",
-    skill:50
-},]
+const backend_skills = [
+  {
+    id: 1,
+    name: "Javascript",
+    skill: 70,
+  },
+  {
+    id: 2,
+    name: "NodeJs",
+    skill: 70,
+  },
+  {
+    id: 3,
+    name: "ExpressJs",
+    skill: 60,
+  },
+  {
+    id: 4,
+    name: "Git",
+    skill: 70,
+  },
+  {
+    id: 5,
+    name: "PostgreSQl",
+    skill: 50,
+  },
+  {
+    id: 6,
+    name: "MongoDB",
+    skill: 50,
+  },
 
-export { skills,backend_skills};
+  {
+    id: 7,
+    name: "Postman",
+    skill: 50,
+  },
+  {
+    id: 8,
+    name: "RestAPI",
+    skill: 50,
+  },
+];
+
+export { skills, backend_skills };
