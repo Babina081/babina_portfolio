da7497def92f1d4f20ddffa4f69bda75e23d6db7
Babina081 <babitamoo333@gmail.com>
2024-06-26 21:33:57 +0545

    update code format of academic section

--- src/components/academic/Academic.jsx
+++ src/components/academic/Academic.jsx
@@ -1,35 +1,35 @@
-import React from 'react';
-import './academic.css';
+import React from "react";
+import "./academic.css";
 import {
   VerticalTimeline,
   VerticalTimelineElement,
-} from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';
-import { FaGraduationCap } from 'react-icons/fa';
-import { ImLocation2 } from 'react-icons/im';
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { FaGraduationCap } from "react-icons/fa";
+import { ImLocation2 } from "react-icons/im";
 // import { PiCaretDoubleDownBold } from 'react-icons/pi';
 
 const data = [
   {
-    date: '2017 - 2022',
-    college: 'Pokhara Engineering College',
-    degree: 'Bachelor in Computer Engineering',
-    location: 'Phire,Pokhara',
-    affiliated: 'Pokhara University',
+    date: "2017 - 2022",
+    college: "Pokhara Engineering College",
+    degree: "Bachelor in Computer Engineering",
+    location: "Phire,Pokhara",
+    affiliated: "Pokhara University",
   },
   {
-    date: '2015-2017',
-    college: 'St. Marys Higher Secondary School ',
-    degree: '+2 Level',
-    location: 'Nayagaun, Pokhara',
-    affiliated: 'Government of Nepal',
+    date: "2015-2017",
+    college: "St. Marys Higher Secondary School ",
+    degree: "+2 Level",
+    location: "Nayagaun, Pokhara",
+    affiliated: "Government of Nepal",
   },
   {
-    date: '2005-2015',
-    college: 'Gyanodaya Bal Batika School',
-    degree: 'School Leaving Certificate (SLC)',
-    location: 'Sanepa, Lalitpur',
-    affiliated: 'Government of Nepal',
+    date: "2005-2015",
+    college: "Gyanodaya Bal Batika School",
+    degree: "School Leaving Certificate (SLC)",
+    location: "Sanepa, Lalitpur",
+    affiliated: "Government of Nepal",
   },
 ];
 const Academic = () => {
@@ -51,32 +51,30 @@ const Academic = () => {
             return (
               <VerticalTimelineElement
                 contentStyle={{
-                  border: '1px solid var(--color-primary)',
+                  border: "1px solid var(--color-primary)",
                 }}
-                // contentArrowStyle={{
-                //   borderRight: '7px solid var(--color-primary) ',
-                // }}
                 className="vertical-timeline-element--education"
                 iconStyle={{
-                  background: 'var(--color-primary)',
-                  color: 'var(--color-white)',
+                  background: "var(--color-primary)",
+                  color: "var(--color-white)",
                 }}
                 icon={<FaGraduationCap />}
                 date={date}
+                key={date}
               >
                 <h2
                   style={{
-                    color: 'var(--color-bg-variant) ',
-                    fontStyle: 'bold',
+                    color: "var(--color-bg-variant) ",
+                    fontStyle: "bold",
                     fontWeight: 800,
                   }}
                 >
                   {college}
                 </h2>
-                <h5 style={{ color: '--color-primary', fontStyle: 'italic' }}>
+                <h5 style={{ color: "--color-primary", fontStyle: "italic" }}>
                   {affiliated}
                 </h5>
-                <h6 style={{ color: 'var(--color-primary)' }}>
+                <h6 style={{ color: "var(--color-primary)" }}>
                   <ImLocation2 />
                   {location}
                 </h6>
@@ -86,7 +84,6 @@ const Academic = () => {
             );
           })}
         </VerticalTimeline>
-       
       </div>
     </section>
   );
