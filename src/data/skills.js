d72972c0b94eb2ad2ade422751bf18a8c1e827e7
Babina081 <babitamoo333@gmail.com>
2024-06-26 21:28:02 +0545

    add images imported and skills structure updated

---
+++ src/data/skills.js
@@ -0,0 +1,259 @@
+import htmlImage from "../../src/assets/technologies/html.png";
+import cssImage from "../../src/assets/technologies/css.png";
+import jsImage from "../../src/assets/technologies/js.png";
+import tsImage from "../../src/assets/technologies/ts.webp";
+import reactImage from "../../src/assets/technologies/react.webp";
+import nextImage from "../../src/assets/technologies/nextjs.png";
+import svelteImage from "../../src/assets/technologies/svelte.png";
+import figmaImage from "../../src/assets/technologies/figma.png";
+import canvaImage from "../../src/assets/technologies/canva.webp";
+import gitImage from "../../src/assets/technologies/git.png";
+import nodeImage from "../../src/assets/technologies/node.png";
+import expressImage from "../../src/assets/technologies/express.png";
+import postgresImage from "../../src/assets/technologies/postgres.png";
+import postmanImage from "../../src/assets/technologies/postman.png";
+import apiImage from "../../src/assets/technologies/api.png";
+import mongoImage from "../../src/assets/technologies/mongo.png";
+import sassImage from "../../src/assets/technologies/sass.png";
+import bootstrapImage from "../../src/assets/technologies/bootstrap.png";
+import reduxImage from "../../src/assets/technologies/redux.png";
+import tailwindImage from "../../src/assets/technologies/tailwind.png";
+import es6Image from "../../src/assets/technologies/es6.png";
+import jqueryImage from "../../src/assets/technologies/jquery.webp";
+import ejsImage from "../../src/assets/technologies/ejs.png";
+
+const skills = [
+  {
+    id: 1,
+    name: "HTML",
+    type: "frontend",
+    img: htmlImage,
+    skill: 80,
+  },
+  {
+    id: 2,
+    name: "CSS",
+    type: "frontend",
+    img: cssImage,
+    skill: 80,
+  },
+  {
+    id: 3,
+    name: "Javascript",
+    type: "frontend",
+    img: jsImage,
+    skill: 70,
+  },
+  {
+    id: 4,
+    name: "Bootstrap",
+    type: "design",
+    img: bootstrapImage,
+    skill: 60,
+  },
+  {
+    id: 5,
+    name: "ReactJs",
+    type: "frontend",
+    img: reactImage,
+    skill: 70,
+  },
+  {
+    id: 6,
+    name: "NextJs",
+    type: "frontend",
+    img: nextImage,
+    skill: 70,
+  },
+  {
+    id: 7,
+    name: "Typescript",
+    type: "frontend",
+    img: tsImage,
+    skill: 70,
+  },
+  {
+    id: 8,
+    name: "Svelte",
+    type: "frontend",
+    img: svelteImage,
+    skill: 70,
+  },
+  {
+    id: 9,
+    name: "Sass",
+    type: "frontend",
+    img: sassImage,
+    skill: 70,
+  },
+
+  {
+    id: 10,
+    name: "ES6",
+    type: "frontend",
+    img: es6Image,
+    skill: 60,
+  },
+  {
+    id: 11,
+    name: "jQuery",
+    type: "frontend",
+    img: jqueryImage,
+    skill: 60,
+  },
+  {
+    id: 12,
+    name: "EJS",
+    type: "frontend",
+    img: ejsImage,
+    skill: 60,
+  },
+  {
+    id: 13,
+    name: "Canva",
+    type: "design",
+    img: canvaImage,
+    skill: 60,
+  },
+  {
+    id: 14,
+    name: "Figma",
+    type: "design",
+    img: figmaImage,
+    skill: 60,
+  },
+
+  {
+    id: 15,
+    name: "Javascript",
+    type: "backend",
+    img: jsImage,
+    skill: 70,
+  },
+  {
+    id: 16,
+    name: "NodeJs",
+    type: "backend",
+    img: nodeImage,
+    skill: 70,
+  },
+  {
+    id: 17,
+    name: "ExpressJs",
+    type: "backend",
+    img: expressImage,
+    skill: 60,
+  },
+  {
+    id: 18,
+    name: "Git",
+    type: "version-control",
+    img: gitImage,
+    skill: 70,
+  },
+  {
+    id: 19,
+    name: "PostgreSQl",
+    type: "backend",
+    img: postgresImage,
+    skill: 50,
+  },
+  {
+    id: 20,
+    name: "MongoDB",
+    type: "backend",
+    img: mongoImage,
+    skill: 50,
+  },
+
+  {
+    id: 21,
+    name: "Postman",
+    type: "testing",
+    img: postmanImage,
+    skill: 50,
+  },
+  {
+    id: 22,
+    name: "RestAPI",
+    type: "backend",
+    img: apiImage,
+    skill: 50,
+  },
+  {
+    id: 23,
+    name: "Redux",
+    type: "frontend",
+    img: reduxImage,
+    skill: 50,
+  },
+  {
+    id: 24,
+    name: "Tailwind",
+    type: "frontend",
+    img: tailwindImage,
+    skill: 50,
+  },
+];
+
+const backend_skills = [
+  {
+    id: 1,
+    name: "Javascript",
+    type: "backend",
+    img: jsImage,
+    skill: 70,
+  },
+  {
+    id: 2,
+    name: "NodeJs",
+    type: "backend",
+    img: nodeImage,
+    skill: 70,
+  },
+  {
+    id: 3,
+    name: "ExpressJs",
+    type: "backend",
+    img: expressImage,
+    skill: 60,
+  },
+  {
+    id: 4,
+    name: "Git",
+    type: "version-control",
+    img: gitImage,
+    skill: 70,
+  },
+  {
+    id: 5,
+    name: "PostgreSQl",
+    type: "backend",
+    img: postgresImage,
+    skill: 50,
+  },
+  {
+    id: 6,
+    name: "MongoDB",
+    type: "backend",
+    img: mongoImage,
+    skill: 50,
+  },
+
+  {
+    id: 7,
+    name: "Postman",
+    type: "testing",
+    img: postmanImage,
+    skill: 50,
+  },
+  {
+    id: 8,
+    name: "RestAPI",
+    type: "backend",
+    img: apiImage,
+    skill: 50,
+  },
+];
+
+export { skills, backend_skills };
